e496ca5c0b6800a0375626fa4bade553bb36a497
Shubham Shrivas <60268544+shubham013333@users.noreply.github.com>
2022-01-31 11:45:16 +0530

    Add files via upload

---
+++ Simple_Interest_28-Jan-22_01.py
@@ -0,0 +1,10 @@
+# Calculate Simple Interst using Python
+
+
+p = int(input("Enter Principle : "))    
+r = int(input("Enter Rate :"))
+t = int(input("Enter Time: "))
+
+SI = p*r*t//100
+
+print(SI)
